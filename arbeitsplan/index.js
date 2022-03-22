f150a911534fd8b15a8169111e278db55bbcbd25
Leon Bartle <leon@bartle.eu>
2022-03-22 16:53:55 +0100

    test

--- arbeitsplan/index.js
+++ arbeitsplan/index.js
@@ -6,12 +6,35 @@ document.getElementById("clickMe").onclick = ausgeben;
 function ausgeben(){
 	const d = new Date();
 	let day = d.getDay();
-	switch(day){
-		case 1: marius();break;
-		case 2: leon();break;
-		case 3: marius();break;
-		case 4: jakob();break;
-		case 5: jakob();break;
+	let week = d.getWeek();
+	switch(week%3){
+		case 0:
+		switch(day){
+				case 1: marius();break;
+				case 2: leon();break;
+				case 3: marius();break;
+				case 4: jakob();break;
+				case 5: jakob();break;}
+			break;
+			
+		case 1:
+		
+			switch(day){
+				case 1: marius();break;
+				case 2: leon();break;
+				case 3: jakob();break;
+				case 4: jakob();break;
+				case 5: marius();break;}
+			break;
+		case 2:
+			switch(day){
+				case 1: marius();break;
+				case 2: jakob();break;
+				case 3: marius();break;
+				case 4: jakob();break;
+				case 5: leon();break;}
+			break;
+			
 	}
 }
 
@@ -24,3 +47,10 @@ function jakob(){
 function marius(){
 	window.alert("Marius");
 }
+
+Date.prototype.getWeek = function() {
+  var onejan = new Date(this.getFullYear(),0,1);
+  var today = new Date(this.getFullYear(),this.getMonth(),this.getDate());
+  var dayOfYear = ((today - onejan + 86400000)/86400000);
+  return Math.ceil(dayOfYear/7)
+};
